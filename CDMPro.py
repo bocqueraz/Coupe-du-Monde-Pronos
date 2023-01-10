5f0461c10a77838a020956eee2ca00673adc15b6
Cyprien Bocqueraz <boccoding@gmail.com>
2023-01-10 22:18:06 +0100

    Debut du programme. Version: pre-alpha

---
+++ CDMPro.py
@@ -0,0 +1,112 @@
+#############################################################################
+#Made by bocqueraz with love!                                               #
+#Source: https://www.fifa.com/fr/fifa-world-ranking/men?dateId=id13792      #
+#License: All Right Reserved                                                #
+#############################################################################
+
+### Importation des modules et de leurs fonctions
+
+from random import randint
+#
+
+### Definition des Equipes et de leurs puissances
+
+Brésil = 1841.3
+
+Belgique = 1816.71
+
+Argentine = 1773.88
+
+France = 1759.78
+
+Angleterre = 1728.47
+
+Italie = 1726.14
+
+Espagne = 1715.22
+
+Pays_Bas = 1694.51
+
+Portugal = 1676.56
+
+Danemark = 1666.57
+
+Allemagne = 1650.21
+
+Croatie = 1645.64
+
+Mexique = 1644.89
+
+Uruguay = 1638.71
+
+Suisse = 1635.92
+
+États_Unis = 1627.48
+
+Colombie = 1611.04
+
+Sénégal = 1584.38
+
+Pays_de_Galles = 1569.82
+
+Republique_Islamique_d0Iran = 1564.61
+
+Serbie = 1563.62
+
+Maroc = 1563.5
+
+Pérou = 1561.01
+
+Japon = 1559.54
+
+Suède = 1553.33
+
+Pologne = 1548.59
+
+Ukraine = 1536.99
+
+République_de_Corée = 1530.3
+
+Chili = 1509.61
+
+Tunisie = 1507.54
+
+Costa_Rica = 1503.59
+
+Nigeria = 1498.47
+
+Autriche = 1492.95
+
+Hongrie = 1492.2
+
+Australie = 1488.72
+
+Canada = 1475
+
+Cameroun = 1471.44
+
+Équateur = 1464.39
+
+Turquie = 1460.14
+
+Côte_d0Ivoire = 1443.06
+
+République_d0Irlande = 1440.19
+
+Qatar = 1439.89
+
+Arabie_saoudite = 1437.78
+
+Ghana = 1393
+#
+
+###Definition des groupes
+
+GroupeA = [{"premier":"","deuxieme":"","troisieme":""},{"Qatar":"","Équateur":"","Senegal":"","Pays Bas":""},"Qatar","Equateur","Senegal","Pays Bas"]
+GroupeB = [{"premier":"","deuxieme":"","troisieme":""},{"Angleterre":"","Republique Islamique d'Iran":"","États Unis":"","Pays de Galles":""},"Qatar","Equateur","Senegal","Pays Bas"]
+GroupeC = [{"premier":"","deuxieme":"","troisieme":""},{"Argentine":"","Arabie Saoudite":"","Mexique":"","Pologne":""},"Qatar","Equateur","Senegal","Pays Bas"]
+GroupeD = [{"premier":"","deuxieme":"","troisieme":""},{"France":"","Australie":"","Danemark":"","Tunisie":""},"Qatar","Equateur","Senegal","Pays Bas"]
+GroupeE = [{"premier":"","deuxieme":"","troisieme":""},{"Espagne":"","Costa Rica":"","Allemagne":"","Japon":""},"Qatar","Equateur","Senegal","Pays Bas"]
+GroupeF = [{"premier":"","deuxieme":"","troisieme":""},{"Belgique":"","Canada":"","Maroc":"","Croatie":""},"Qatar","Equateur","Senegal","Pays Bas"]
+GroupeG = [{"premier":"","deuxieme":"","troisieme":""},{"Bresil":"","Serbie":"","Suisse":"","Cameroun":""},"Qatar","Equateur","Senegal","Pays Bas"]
+GroupeH = [{"premier":"","deuxieme":"","troisieme":""},{"Portugal":"","Ghana":"","Uruguay":"","République de Corée":""},"Qatar","Equateur","Senegal","Pays Bas"]
